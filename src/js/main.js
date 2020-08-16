6f3265e52ef974c2d25289128d555e0e485d4b72
Alekseev Konstantin <alexcseev1990@gmail.com>
2020-08-16 14:18:28 +0300

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ import MainSlider from "./modules/slider/slider-main";
 import VideoPlayer from "./modules/playVideo";
 import MiniSlider from "./modules/slider/slider-mini";
 import Difference from "./modules/difference.js";
+import Form from "./modules/form";
+import ShowInfo from "./modules/showInfo";
+import Download from "./modules/download";
 
 window.addEventListener("DOMContentLoaded", () => {
   const slider = new MainSlider({ btns: ".next", container: ".page" });
@@ -26,6 +29,12 @@ window.addEventListener("DOMContentLoaded", () => {
   });
   modulesSlider.init();
 
+  const moduleSlider = new MainSlider({
+    container: ".moduleapp",
+    btns: ".next",
+  });
+  moduleSlider.render();
+
   const feedSlider = new MiniSlider({
     container: ".feed__slider",
     prev: ".feed__slider .slick-prev",
@@ -34,8 +43,14 @@ window.addEventListener("DOMContentLoaded", () => {
   });
   feedSlider.init();
 
-  const player = new VideoPlayer(".showup .play", ".overlay");
-  player.init();
+  new VideoPlayer(".showup .play", ".overlay").init();
+  new VideoPlayer(".module__video-item .play", ".overlay").init();
 
   new Difference(".officerold", ".officernew", ".officer__card-item").init();
+
+  new Form(".form").init();
+
+  new ShowInfo(".plus__content").init();
+
+  new Download(".download").init();
 });
